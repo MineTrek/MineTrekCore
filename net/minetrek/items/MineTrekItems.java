f9246ae381b3611da938b0f0fe75504edde75996
Francesco Macagno <fjmacagno1@yahoo.com>
2014-01-19 16:01:02 -0800

    Added Small Antimatter Storage

--- net/minetrek/items/MineTrekItems.java
+++ net/minetrek/items/MineTrekItems.java
@@ -52,101 +52,79 @@ public class MineTrekItems {
 	public static Item rubble;
 	public static Item dilithium_crystal;
 	public static Item phaser;
+	public static Item smallAntimatterStorage;
 
-	public static int initialize(int startID, Configuration conf,
-			CreativeTabs tab) {
+	public static int initialize(int startID, Configuration conf, CreativeTabs tab) {
 
 		// Ingots
-		aluminum_ingot = new DilithiumIngot(conf.getItem("AluminumIngot",
-				startID++).getInt()).setCreativeTab(tab);
+		aluminum_ingot = new DilithiumIngot(conf.getItem("AluminumIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(aluminum_ingot, "Aluminum Ingot");
 
-		dilithium_ingot = new DilithiumIngot(conf.getItem("DilithiumIngot",
-				startID++).getInt()).setCreativeTab(tab);
+		dilithium_ingot = new DilithiumIngot(conf.getItem("DilithiumIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(dilithium_ingot, "Dilithium Ingot");
 
-		bronze_ingot = new BronzeIngot(conf.getItem("BronzeIngot", startID++)
-				.getInt()).setCreativeTab(tab);
+		bronze_ingot = new BronzeIngot(conf.getItem("BronzeIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(bronze_ingot, "Bronze Ingot");
 
-		brass_ingot = new BrassIngot(conf.getItem("BrassIngot", startID++)
-				.getInt()).setCreativeTab(tab);
+		brass_ingot = new BrassIngot(conf.getItem("BrassIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(brass_ingot, "Brass Ingot");
 
-		chromium_ingot = new ChromiumIngot(conf.getItem("ChromiumIngot",
-				startID++).getInt()).setCreativeTab(tab);
+		chromium_ingot = new ChromiumIngot(conf.getItem("ChromiumIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(chromium_ingot, "Chromium Ingot");
 
-		copper_ingot = new CopperIngot(conf.getItem("CopperIngot", startID++)
-				.getInt()).setCreativeTab(tab);
+		copper_ingot = new CopperIngot(conf.getItem("CopperIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(copper_ingot, "Copper Ingot");
 
-		duranium_ingot = new DuraniumIngot(conf.getItem("DuraniumIngot",
-				startID++).getInt()).setCreativeTab(tab);
+		duranium_ingot = new DuraniumIngot(conf.getItem("DuraniumIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(duranium_ingot, "Duranium Ingot");
 
-		invisible_ingot = new InvisibleIngot(conf.getItem("InvisibleIngot",
-				startID++).getInt()).setCreativeTab(tab);
+		invisible_ingot = new InvisibleIngot(conf.getItem("InvisibleIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(invisible_ingot, "Invisible Ingot");
 
-		iridium_ingot = new IridiumIngot(conf
-				.getItem("IridiumIngot", startID++).getInt())
-				.setCreativeTab(tab);
+		iridium_ingot = new IridiumIngot(conf.getItem("IridiumIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(iridium_ingot, "Iridium Ingot");
 
-		platinum_ingot = new PlatinumIngot(conf.getItem("PlatinumIngot",
-				startID++).getInt()).setCreativeTab(tab);
+		platinum_ingot = new PlatinumIngot(conf.getItem("PlatinumIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(platinum_ingot, "Platinum Ingot");
 
-		plutonium_ingot = new PlutoniumIngot(conf.getItem("PlutoniumIngot",
-				startID++).getInt()).setCreativeTab(tab);
+		plutonium_ingot = new PlutoniumIngot(conf.getItem("PlutoniumIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(plutonium_ingot, "Plutonium Ingot");
 
-		silicon_ingot = new SiliconIngot(conf
-				.getItem("SiliconIngot", startID++).getInt())
-				.setCreativeTab(tab);
+		silicon_ingot = new SiliconIngot(conf.getItem("SiliconIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(silicon_ingot, "Silicon Ingot");
 
-		steel_ingot = new SteelIngot(conf.getItem("SteelIngot", startID++)
-				.getInt()).setCreativeTab(tab);
+		steel_ingot = new SteelIngot(conf.getItem("SteelIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(steel_ingot, "Steel Ingot");
 
-		tin_ingot = new TinIngot(conf.getItem("TinIngot", startID++).getInt())
-				.setCreativeTab(tab);
+		tin_ingot = new TinIngot(conf.getItem("TinIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(tin_ingot, "Tin Ingot");
 
-		titanium_ingot = new TitaniumIngot(conf.getItem("TitaniumIngot",
-				startID++).getInt()).setCreativeTab(tab);
+		titanium_ingot = new TitaniumIngot(conf.getItem("TitaniumIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(titanium_ingot, "Titanium Ingot");
 
-		tritanium_ingot = new TritaniumIngot(conf.getItem("TritaniumIngot",
-				startID++).getInt()).setCreativeTab(tab);
+		tritanium_ingot = new TritaniumIngot(conf.getItem("TritaniumIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(tritanium_ingot, "Tritanium Ingot");
 
-		tungsten_ingot = new TungstenIngot(conf.getItem("TungstenIngot",
-				startID++).getInt()).setCreativeTab(tab);
+		tungsten_ingot = new TungstenIngot(conf.getItem("TungstenIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(tungsten_ingot, "Tungsten Ingot");
 
-		uranium_ingot = new UraniumIngot(conf
-				.getItem("UraniumIngot", startID++).getInt())
-				.setCreativeTab(tab);
+		uranium_ingot = new UraniumIngot(conf.getItem("UraniumIngot", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(uranium_ingot, "Uranium Ingot");
 
 		// Dust
 
 		// Tools
-		phaser = new Phaser(conf.getItem("Phaser", startID++).getInt())
-				.setCreativeTab(tab);
+		phaser = new Phaser(conf.getItem("Phaser", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(phaser, "phaser");
 
 		// Others
-		rubble = new Rubble(conf.getItem("Rubble", startID++).getInt())
-				.setCreativeTab(tab);
+		rubble = new Rubble(conf.getItem("Rubble", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(rubble, "Rubble");
 
-		dilithium_crystal = new DilithiumCrystal(conf.getItem(
-				"DilithiumCrystal", startID++).getInt()).setCreativeTab(tab);
+		dilithium_crystal = new DilithiumCrystal(conf.getItem("DilithiumCrystal", startID++).getInt()).setCreativeTab(tab);
 		LanguageRegistry.addName(dilithium_crystal, "Dilithium Crystal");
+
+		smallAntimatterStorage = new SmallAntimatterStorage(conf.getItem("SmallAntimatterStorage", startID++).getInt()).setCreativeTab(tab);
 
 		return startID;
 
--- net/minetrek/items/SmallAntimatterStorage.java
+++ net/minetrek/items/SmallAntimatterStorage.java
@@ -2,25 +2,40 @@ package net.minetrek.items;
 
 import java.util.List;
 
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class SmallAntimatterStorage extends Item {
 
+	private Icon empty;
+	private Icon full;
+
 	public SmallAntimatterStorage(int par1) {
 		super(par1);
 		setMaxStackSize(64);
 		setUnlocalizedName("smallAntimatterStorage");
-		setTextureName("minetrek:dilithiumCrystal");
 		setHasSubtypes(true);
 		setMaxDamage(0);
 	}
 
 	@Override
-	public String getItemStackDisplayName(ItemStack is) {
+	public Icon getIconFromDamage(int dam) {
+		return dam == 1 ? full : empty;
+	}
+
+	@Override
+	public void registerIcons(IconRegister ir) {
+		empty = ir.registerIcon("minetrek:smallAntimatterStorage_empty");
+		full = ir.registerIcon("minetrek:smallAntimatterStorage_full");
+	}
+
+	@Override
+	public String getItemDisplayName(ItemStack is) {
 		switch (is.getItemDamage()) {
 		case 1:
 			return "Full Small Antimatter Storage";
